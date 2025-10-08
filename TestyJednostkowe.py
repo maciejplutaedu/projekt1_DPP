b255bc2cf704643f2313255c4de858b6696f1b52
maciejplutaedu <maciej.pluta@edu.uekat.pl>
2025-10-08 11:20:16 +0200

    test

--- TestyJednostkowe.py
+++ TestyJednostkowe.py
@@ -1,1 +1,32 @@
-#test
+def isPalindrome(word):
+    word = word.lower().replace(" ","")
+    wordReversed = word[::-1]
+    if word==wordReversed:
+        print(wordReversed)
+        return True
+    else:
+        return False
+
+def fibbonaci(num):
+    num=num-1
+    a,b =0,1
+    for i in range(num):
+        a,b=b,a+b
+
+    return a
+
+def count_vowels(word):
+    sum=0
+    word=word.lower()
+    for l in word:
+        if l in "aeiouy":
+            sum+=1
+    return sum
+
+def calculate_discount(price, discount):
+    if discount<0 or discount>1:
+        raise ValueError("Niepoprawna znizka")
+    discount=1-discount
+    price=price*discount
+    return price
+
